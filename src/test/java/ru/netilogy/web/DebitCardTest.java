101d6420fe4fd9448ed04fb974baa0e459e3ef51
YuliaYuzhakova <JuliaU33@mail.ru>
2022-08-17 16:46:38 +0700

    Added Tests

--- src/test/java/ru/netilogy/web/DebitCardTest.java
+++ src/test/java/ru/netilogy/web/DebitCardTest.java
@@ -3,25 +3,55 @@ package ru.netilogy.web;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.List;
+
 
 public class DebitCardTest {
 
     private WebDriver driver;
 
     @BeforeAll
-    static void setUp1 (){
+    static void setUp1() {
         System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
     }
 
+    @Test
+    void debitCardTest1() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Иван");
+        elements.get(1).sendKeys("+79135504488");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button_view_extra")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+        Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+    @Test
+    void debitCardTest2() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("span[data-test-id=\"name\"] input")).sendKeys("Иван");
+        driver.findElement(By.cssSelector("span[data-test-id=\"phone\"] input")).sendKeys("+79135504488");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button_view_extra")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+        Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
     @BeforeEach
-    void setUp2(){
+    void setUp2() {
         driver = new ChromeDriver();
     }
 
     @AfterEach
-    public void close(){
+    public void close() {
         driver.quit();
         driver = null;
     }
