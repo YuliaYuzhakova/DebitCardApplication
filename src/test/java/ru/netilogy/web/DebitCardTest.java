c2cc70a6da46b0218711d511ea14a3987811a85d
YuliaYuzhakova <JuliaU33@mail.ru>
2022-08-19 17:06:26 +0700

    Changed Test

--- src/test/java/ru/netilogy/web/DebitCardTest.java
+++ src/test/java/ru/netilogy/web/DebitCardTest.java
@@ -26,7 +26,7 @@ public class DebitCardTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-        driver.get(" http://localhost:9999");
+        driver.get("http://localhost:9999");
     }
 
     @AfterEach
@@ -38,24 +38,22 @@ public class DebitCardTest {
 
     @Test
     void debitCardTest1() {
-        driver.get("http://localhost:9999");
         List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Иван");
+        elements.get(0).sendKeys("Дмитрий Мамин-Сибиряк");
         elements.get(1).sendKeys("+79135504488");
-        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
         driver.findElement(By.className("button_view_extra")).click();
-        String text = driver.findElement(By.className("paragraph")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id=\"order-success\"]")).getText();
         Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
     @Test
     void debitCardTest2() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("span[data-test-id=\"name\"] input")).sendKeys("Иван");
+        driver.findElement(By.cssSelector("span[data-test-id=\"name\"] input")).sendKeys("Дмитрий Мамин-Сибиряк");
         driver.findElement(By.cssSelector("span[data-test-id=\"phone\"] input")).sendKeys("+79135504488");
-        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
         driver.findElement(By.className("button_view_extra")).click();
-        String text = driver.findElement(By.className("paragraph")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id=\"order-success\"]")).getText();
         Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 }
